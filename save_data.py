6710327ce003efb85b363c40d0e3a6e04965ccd2
hvate <itsposs@163.com>
2021-12-17 17:18:58 +0800

    fix class

--- save_data.py
+++ save_data.py
@@ -1,15 +1,27 @@
-import tushare as ts
-import pymysql
-import pandas as pd
-pymysql.install_as_MySQLdb()
-from sqlalchemy import create_engine
 
-pro = ts.pro_api('ff08684f06ec5a81164471fd1e1c2cca1951bca5fbe34f155ca221fb')
-df = pro.daily(start_date='20210901', end_date='20211216')
-#print(df)
-daily_all = create_engine('mysql+pymysql://root:123456@localhost/tushare')
-res = df.to_sql('stock',daily_all,index=False,if_exists='append',chunksize=5000)
-sql = 'select * from stock'
-df = pd.read_sql_query(sql, daily_all)
-# print(tushare.__version__)
+#!/usr/bin/python3
+#vim: setfileencoding=utf-8:
+#filename: save_data.py
+
+import pandas as pd
+import tushare as ts
+
+TOKEN = 'ff08684f06ec5a81164471fd1e1c2cca1951bca5fbe34f155ca221fb'
+
+class SaveData:
+	def __init__(self):
+		self._pro = ts.pro_api(TOKEN)
+		self._ver = ts.__version__
+		print('tushare version: %s.', str(self._ver))
+	
+	def daily_data(self,start_date, end_date):
+		df = self._pro.daily(start_date=start_date, end_date=end_date)
+		print(df)
+
+	
+if __name__ == "__main__":
+	beg_time = '20210901'
+	end_time = '20211216'
+	s = SaveData()
+	s.daily_data(start_date=beg_time, end_date=end_time)
 
